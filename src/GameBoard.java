b82c565cb0baf99ae7bc655b10577c79752bb06f
Steven Shelafoe <sshelafo@asu.edu>
2025-11-10 19:01:10 -0700

    Add GameBoard class with initialization and display
    - GameBoard manages 3x3 grid state
    - Initialization sets all cells to empty
    - Display method shows current board state

---
+++ src/GameBoard.java
@@ -0,0 +1,25 @@
+public class GameBoard {
+    private char[][] board;
+    
+    public GameBoard() {
+        board = new char;
+        initializeBoard();
+    }
+    
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+    
+    public void displayBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
