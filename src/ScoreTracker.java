0139a667cb11ec5911cb3ab327f04e855e787538
Steven Shelafoe <sshelafo@asu.edu>
2025-11-10 19:03:14 -0700

    Add ScoreTracker class with getter methods
    - Tracks scores for both players
    - Increment methods for updating scores
    - Getter methods for retrieving current scores

---
+++ src/ScoreTracker.java
@@ -0,0 +1,21 @@
+public class ScoreTracker {
+    private int player1Score;
+    private int player2Score;
+    
+    public void incrementPlayer1() {
+        player1Score++;
+    }
+    
+    public void incrementPlayer2() {
+        player2Score++;
+    }
+}
+    
+    public int getPlayer1Score() {
+        return player1Score;
+    }
+    
+    public int getPlayer2Score() {
+        return player2Score;
+    }
+}
